b65f87c7d05f8b2ae70c279dd3e3ee42f00e25fb
Ford Henley <info@fordswebhelp.com>
2025-11-17 10:57:00 -0500

    Added comments to build out the logic structure

---
+++ Skyjo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Skyjo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Build The Deck into a array holding all the cards in the game -- maybe half for a one player game
+
+            //Shuffle The Deck by randomizing the array
+
+            //Deal The Cards to the user into an array
+
+            //Assign 12 cards to user in an array 
+
+            //Randomize the order of the desk to simulated laying out your cards on the table
+
+            //Print deck to the console in a 4 x 3 matrix of 12 cards displaying the cards as an x to represent the back of the card
+
+            /**** Start The Loop ***/
+
+            //Ask user to pick a card from deck or or to flip a card in their hand
+
+            //The user picks an index in their deck array and then the deck is printed to the console to reveal their card number
+
+            /**** Repeat The Loop Until all 12 cards are revealed -- possibly add a way to take cards from the deck to change out the array ***/
+
+            //Total the cards and print the users point total to the console
+
+
+        }
+    }
+}
