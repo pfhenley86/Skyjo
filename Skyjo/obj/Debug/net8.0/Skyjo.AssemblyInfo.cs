11bace2457e9ca8e24459a9258d0243da96f09a4
“pfhenley86” <fordp86@gmail.com>
2026-01-07 16:25:40 -0500

    updated repo

--- Skyjo/obj/Debug/net8.0/Skyjo.AssemblyInfo.cs
+++ Skyjo/obj/Debug/net8.0/Skyjo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Skyjo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da74bf6c623e1cd4640f03756a4cc3a87e586ff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+919c5bdf9b6e8dcd44cfc86380953b6a891993a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Skyjo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Skyjo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
